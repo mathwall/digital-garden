f5249fae50284a8dfc2c5e39cceb4c9359bea9e3
Mathilde Sauvage <wallon.mathilde@gmail.com>
2025-09-25 17:55:33 +0200

    style: update to Maxi Ferreira theme

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,15 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-		}),
+const posts = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/data/blog-posts" }),
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    publishDate: z.union([z.string(), z.date()]),
+    description: z.string(),
+  }),
 });
 
-export const collections = { blog };
+export const collections = { posts };
