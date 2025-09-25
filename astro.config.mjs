ed30eca82039d2dd552b5832860a7da586309745
Mathilde Sauvage <wallon.mathilde@gmail.com>
2025-09-25 17:55:33 +0200

    style: update to Maxi Ferreira theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
-
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mathwall.github.io",
   //   base: "/digital-garden",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord",
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
